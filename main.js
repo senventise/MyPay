3cfadc7a790916ffc553e57da23163af53067594
senventise <2375741859@qq.com>
2019-05-31 21:02:34 +0800

    恢复了原来的文件样式

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alipayURL="HTTPS://QR.ALIPAY.COM/FKX00576LNYX64NDCHUH7A";wechat="MicroMessenger";alipay="AlipayClient";qqBrowser="MQQBrowser";userAgent=navigator.userAgent;qqSwitch=true;timSwitch=true;function inUA(s){return userAgent.indexOf(s)!=-1}if(inUA(wechat)){window.location.href="static/wechat.html"}else{if(inUA(alipay)){window.location.href=alipayURL}else{if(inUA(qqBrowser)){if(timSwitch&&inUA("TIM")){window.location.href="static/tim.html"}else{if(qqSwitch){window.location.href="static/qq.html"}}}else{alert("无法识别的客户端！");window.location.href="static/error.html"}}};
+alipayURL="HTTPS://QR.ALIPAY.COM/FKX00576LNYX64NDCHUH7A";
+wechat="MicroMessenger";
+alipay="AlipayClient";
+qqBrowser="MQQBrowser";
+userAgent=navigator.userAgent;
+qqSwitch=true;
+timSwitch=true;
+
+function inUA(s){
+    return userAgent.indexOf(s)!=-1
+}
+
+if(inUA(wechat)){
+    window.location.href="static/wechat.html";
+}else if(inUA(alipay)){
+    window.location.href=alipayURL;
+}else if(inUA(qqBrowser)){
+    if(timSwitch&&inUA("TIM")){
+        window.location.href="static/tim.html";
+    }else{
+        if(qqSwitch){
+        window.location.href="static/qq.html";
+    }
+}
+}else{
+    alert("无法识别的客户端！");
+    window.location.href="static/error.html";
+}
--- static/check.js
+++ static/check.js
@@ -1,13 +1,9 @@
 //判断是否是非法访问
-function getUA(){
-   return navigator.userAgent;
-  }
-
 wechat = "MicroMessenger";
 alipay = "AlipayClient";
 qqBrowser = "MQQBrowser"
-userAgent = getUA();
+userAgent = navigator.userAgent;
 
 if (userAgent.indexOf(wechat)==-1 && userAgent.indexOf(alipay)==-1 && userAgent.indexOf(qqBrowser)==-1){
-  window.location.href = "index.html"
+  window.location.href = "/index.html"
 }
